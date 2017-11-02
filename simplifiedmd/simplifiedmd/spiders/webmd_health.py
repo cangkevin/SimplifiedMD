4494695e1b0c37eca939c02a340346598932f2d4
Kevin <cangkevin@gmail.com>
2017-11-01 19:27:08 -0500

    refactored code

--- simplifiedmd/simplifiedmd/spiders/webmd_health.py
+++ simplifiedmd/simplifiedmd/spiders/webmd_health.py
@@ -2,36 +2,42 @@ import scrapy
 from scrapy import Request
 from bs4 import BeautifulSoup
 import bleach
+import webmd_health_const as CONST
 
 class WebMDSpider(scrapy.Spider):
-	name = "webmd_health"
+	name = 'webmd_health'
 	start_urls = [
-        'https://www.webmd.com/a-to-z-guides/health-topics',
+        CONST.WEBMD_BASE_URL
     ]
 	
 	# follow links to next alphabet page
 	def parse(self, response):
-		a2z = []
-		for i in range (97, 123):
-			a2z.append("https://www.webmd.com/a-to-z-guides/health-topics" + "?pg=" + chr(i))
-		for j in range(len(a2z)):
-			yield Request(response.urljoin(a2z[j]), callback = self.parse_dis)
+		letter_urls = []
+
+		for letter in range(CONST.ASCII_LETTER_A, CONST.ASCII_LETTER_Z):
+			letter_urls.append(CONST.WEBMD_BASE_URL + CONST.WEBMD_ALPHABET_EXT + chr(letter))
+
+		for url in letter_urls:
+			yield Request(response.urljoin(url), callback = self.parse_topics)
 	
-	# go through the diseases on the alpha page
-	def parse_dis(self, response):
-		alpha = response.css('ul.az-index-results-group-list a::attr(href)').extract()
-		for i in range(len(alpha)):
-			yield Request(response.urljoin(alpha[i]), callback = self.parse_page)
+	# go through the topics on the alphabet page
+	def parse_topics(self, response):
+		topics = response.css('ul.az-index-results-group-list a::attr(href)').extract()
+
+		for topic in topics:
+			yield Request(response.urljoin(topic), callback = self.parse_page)
 	
-	# parse article
+	# parse a topice page
 	def parse_page(self, response):
-		txt_str = ""
-		for page in response.css('div.article-page'):
-			for paragraph in page.css('p').extract():
-				tmp = paragraph.strip()
-				tmp = bleach.clean(tmp, tags=[], strip=True)
-				txt_str += tmp
-		yield {
-			'title': response.css('title::text').extract_first(),
-			'text': txt_str,
-		}
+            txt_str = ''
+            for page in response.css('div.article-page'):
+                for paragraph in page.css('p').extract():
+                    tmp = paragraph.strip()
+                    tmp = bleach.clean(tmp, tags=[], strip=True)
+                    txt_str += ' ' + tmp
+
+            if len(txt_str) > 0:
+                yield {
+                    'title': response.css('title::text').extract_first(),
+                    'text': ' '.join(txt_str.strip().split()),
+                }
---
+++ simplifiedmd/simplifiedmd/spiders/webmd_health_const.py
@@ -0,0 +1,6 @@
+WEBMD_BASE_URL = 'https://www.webmd.com/a-to-z-guides/health-topics'
+
+WEBMD_ALPHABET_EXT = '?pg='
+
+ASCII_LETTER_A = 97
+ASCII_LETTER_Z = 123
