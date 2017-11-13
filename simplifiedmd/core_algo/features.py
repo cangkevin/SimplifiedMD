7152ba7b4570992ef68d3ad7fe3e8f87cf0308d0
Kevin <cangkevin@gmail.com>
2017-11-13 14:54:23 -0600

    refactored code

--- simplifiedmd/core_algo/features.py
+++ simplifiedmd/core_algo/features.py
@@ -2,22 +2,28 @@ from __future__ import division
 import math
 from math import cos
 
-## Code taken from: http://pythoncentral.io/how-to-check-if-a-string-is-a-number-in-python-including-unicode/
+"""
+    Helper Functions for computing features
+"""
 def is_number(s):
     try:
         float(s)
         return True
     except ValueError:
-        pass
- 
-    try:
-        import unicodedata
-        unicodedata.numeric(s)
-        return True
-    except (TypeError, ValueError):
-        pass
-	return False
+        return False
 
+def compute_term_freq(sent):
+    term_freq = {}
+    for word in sent.split():
+        if word in term_freq:
+            term_freq[word] += 1
+        else:
+            term_freq[word] = 1
+    return term_freq
+
+"""
+    Functions to compute features
+"""
 def theme_count(sent, theme_words):
     count = 0
     for word in sent.split():
@@ -64,54 +70,34 @@ def count_numerals(sent):
 	return count/len(sent)
 
 def compute_tfisf(sent, transformed_text):
-	term_freq = {}
-	for word in sent.split():
-		if word in term_freq:
-			term_freq[word] += 1
-		else:
-			term_freq[word] = 1
-	
-	sum = 0
-	
-	for key, value in term_freq.iteritems():
-		sent_freq = 0
-		for sentence in transformed_text:
-			sentence_list = sentence.split()
-			if key in sentence_list and (sentence != sent):
-				sent_freq += 1
-		if sent_freq > 0:
-			sum += term_freq[key] * (len(transformed_text)-1/sent_freq)
-	if sum > 0:
-		return math.log10(sum)/len(term_freq)
-	else:
-		return 0
-	
+    term_freq = compute_term_freq(sent)
+    sum = 0
+    for key, value in term_freq.iteritems():
+        sent_freq = 0
+        for sentence in transformed_text:
+            wordlist = sentence.split()
+            if key in wordlist and (sentence != sent):
+                sent_freq += 1
+        if sent_freq > 0:
+            sum += term_freq[key] * (len(transformed_text)-1/sent_freq)
+    if sum > 0:
+        return math.log10(sum)/len(term_freq)
+    else:
+        return 0
 
 def compute_centroid_sim(sent, centroid):
-	centroid_freq = {}
-	for word in centroid.split():
-		if word in centroid_freq:
-			centroid_freq[word] += 1
-		else:
-			centroid_freq[word] = 1
-			
-	sent_freq = {}
-	for word in sent.split():
-		if word in sent_freq:
-			sent_freq[word] += 1
-		else:
-			sent_freq[word] = 1
-	
-	numerator = 0
-	if len(centroid_freq) > len(sent_freq):
-		for key, value in sent_freq.iteritems():
-			if key in centroid_freq:
-				numerator += value * centroid_freq[key]
-	else:
-		for key, value in centroid_freq.iteritems():
-			if key in sent_freq:
-				numerator += value * sent_freq[key]
-	
+    centroid_freq = compute_term_freq(centroid)
+    sent_freq = compute_term_freq(sent)
+    numerator = 0
+    if len(centroid_freq) > len(sent_freq):
+        for key, value in sent_freq.iteritems():
+            if key in centroid_freq:
+                numerator += value * centroid_freq[key]
+    else:
+        for key, value in centroid_freq.iteritems():
+            if key in sent_freq:
+                numerator += value * sent_freq[key]
+                
 	sqrt_centroid = 0
 	for key, value in centroid_freq.iteritems():
 		sqrt_centroid += value ** 2
--- simplifiedmd/core_algo/main.py
+++ simplifiedmd/core_algo/main.py
@@ -10,7 +10,7 @@ import json
 import operator
 from features import *
 
-example_text = "Alkaptonuria is a rare genetic metabolic disorder characterized by the accumulation of homogentisic acid in the body. Affected individuals lack enough functional levels of an enzyme required to breakdown homogentisic acid. Affected individuals may have dark urine or urine that turns black when exposed to air. However, this change may not occur for several hours after urination and often goes unnoticed. Aside from dark urine that is present from infancy, affected individuals generally do not develop symptoms (asymptomatic) during infancy or childhood and often remain unaware of their condition until adulthood. Affected individuals eventually develop ochronosis, which is the bluish-black discoloration of connective and other tissue within the body. Affected individuals may develop discoloration of the skin overlying cartilage within the body such as over part of the outer ear. In some cases, the whites of the eyes (sclera) may also become discolored. In adulthood, affected individuals also develop progressive arthritis of the spine and large joints. The HDG gene codes for the enzyme required for the breakdown of homogentisic acid. Mutations in HDG cause alkaptonuria. Adding more random sentences. Testing stuff yay!"
+example_text = "Alkaptonuria is a rare genetic metabolic disorder characterized by the accumulation of homogentisic acid in the body. Affected individuals lack enough functional levels of an enzyme required to breakdown homogentisic acid. Affected individuals may have dark urine or urine that turns black when exposed to air. However, this change may not occur for several hours after urination and often goes unnoticed. Aside from dark urine that is present from infancy, affected individuals generally do not develop symptoms (asymptomatic) during infancy or childhood and often remain unaware of their condition until adulthood. Affected individuals eventually develop ochronosis, which is the bluish-black discoloration of connective and other tissue within the body. Affected individuals may develop discoloration of the skin overlying cartilage within the body such as over part of the outer ear. In some cases, the whites of the eyes (sclera) may also become discolored. In adulthood, affected individuals also develop progressive arthritis of the spine and large joints. The HDG gene codes for the enzyme required for the breakdown of homogentisic acid. Mutations in HDG cause alkaptonuria. Adding more random sentences. Testing stuff yay! People over age of 50 get alkaptonuria."
 
 stemmer = PorterStemmer()
 stop_words = set(stopwords.words('english'))
@@ -52,11 +52,11 @@ norm_word_freq = sorted(norm_word_freq.iteritems(), key=operator.itemgetter(1), 
 theme_words = [x[0] for x in norm_word_freq[:10]]
 
 # find the centroid sentence
-tfisf_scores = {}
+tfisf_computations = {}
 for sentence in transformed_text:
 	score = compute_tfisf(sentence, transformed_text)
-	tfisf_scores[sentence] = score
-tfisf_scores = sorted(tfisf_scores.iteritems(), key=operator.itemgetter(1), reverse=True)
+	tfisf_computations[sentence] = score
+tfisf_scores = sorted(tfisf_computations.iteritems(), key=operator.itemgetter(1), reverse=True)
 
 # create the sentence-feature matrix
 sent_feat_mat = np.zeros(shape=(len(transformed_text), 8))
@@ -68,7 +68,7 @@ for i in range(len(transformed_text)):
                         sent_paragraph_pos(i, len(transformed_text)), 
                         count_proper(transformed_text[i], normalized_words_pos), 
                         count_numerals(transformed_text[i]), 
-                        compute_tfisf(transformed_text[i], transformed_text), 
+                        tfisf_computations[transformed_text[i]], 
                         compute_centroid_sim(transformed_text[i], tfisf_scores[0][0])]
 
 print sent_feat_mat
